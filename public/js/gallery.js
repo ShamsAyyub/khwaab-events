253894020ca1ec863b2fb9d34380766bc733fe97
ShamsAyyub <shams.ayyub@gmail.com>
2025-12-23 21:50:51 -0500

    Update gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,1 +1,15 @@
+const galleryItems = document.querySelectorAll(".gallery-item");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close");
 
+galleryItems.forEach(item => {
+  item.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = item.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
